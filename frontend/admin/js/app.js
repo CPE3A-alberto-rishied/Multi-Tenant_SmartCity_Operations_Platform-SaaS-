a3be0cab918fcd45677c7637543b808936fa2aba
CPE3A-migallos-arvin <2023103285@ms.bulsu.edu.ph>
2026-04-21 17:04:29 +0800

    Update app.js

--- frontend/admin/js/app.js
+++ frontend/admin/js/app.js
@@ -65,7 +65,7 @@ async function handleLogin(event) {
 
     try {
         // Change this URL if your backend is hosted somewhere else
-        const response = await fetch('http://localhost:3000/api/login', {
+        const response = await fetch('https://beat-pasig-api.onrender.com/api/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
